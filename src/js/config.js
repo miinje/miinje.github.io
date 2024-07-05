239fd2df232443fde8d432b047e3daf152d1a5e2
minjee <134564306+miinje@users.noreply.github.com>
2024-07-06 01:59:40 +0900

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+// [주의] 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
+export const firebaseConfig = {
+  apiKey: "AIzaSyADpjt1Rb3SXtwMPTzihnhfWAe7yhK8glw",
+  authDomain: "vaco-messenger-aeb47.firebaseapp.com",
+  projectId: "vaco-messenger-aeb47",
+  storageBucket: "vaco-messenger-aeb47.appspot.com",
+  messagingSenderId: "910242138326",
+  appId: "1:910242138326:web:4df82a6a60bc712ddc2177",
+  databaseURL:
+    "https://vaco-messenger-aeb47-default-rtdb.asia-southeast1.firebasedatabase.app",
+};
