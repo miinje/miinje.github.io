5031378e42cb34d2e338ed212d16a67d110393b4
minjee <134564306+miinje@users.noreply.github.com>
2024-07-06 02:25:27 +0900

    Add files via upload

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-export function formatDate (dateString) {
+export function formatDate(dateString) {
   const d = new Date(dateString);
 
   const month = d.getMonth() + 1;
@@ -6,14 +6,15 @@ export function formatDate (dateString) {
 
   let hours = d.getHours();
   let minutes = d.getMinutes();
+  let seconds = d.getSeconds();
   const ampm = hours >= 12 ? "오후" : "오전";
   hours = hours % 12;
   hours = hours ? hours : 12;
-  const time = `${ampm} ${hours}시 ${minutes}분`;
+  const time = `${ampm} ${hours}시 ${minutes}분 ${seconds}초`;
 
   return `${month}월 ${date}일 ${time}`;
 }
 
-export function generateHexCode () {
-  return `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, "0")}`;
+export function generateHexCode() {
+  return `#${((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0")}`;
 }
